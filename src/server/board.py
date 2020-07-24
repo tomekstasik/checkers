2de5c41fb603916834a72573c9332bd654c0f61b
Tomek Stasik <tomekstasik01@gmail.com>
2020-07-24 20:50:26 +0200

    Initail revision

---
+++ src/server/board.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from pawn import (BlackPawn, WhitePawn)
+from move import Move
+
+class Board(object):
+    def __init__(self, board):
+        """Initialize current board"""
+        pass
+
+    def generate(self):
+        """Generates start board"""
+        pass
+
+    def make_move(self, move):
+        """Try to make move from current_position to destination"""
+        pass
+
+    def is_legal_move(self, move):
+        """Checks if current move is leagal"""
+        pass
+
+    def is_move_within_board(self, move):
+        """Checks if move is within bound of board"""
+        pass
---
+++ src/server/move.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+class Move(object):
+    def __init__(self, current_position, destination):
+        """Initilize move items"""
+        pass
+
+    def translate_to_xy(self):
+        """Translates client move representation to board coordinates:
+          A B C D E F G H
+        8[               ]8
+        7[               ]7
+        6[               ]6
+        5[               ]5
+        4[               ]4
+        3[               ]3
+        2[               ]2
+        1[               ]1
+          A B C D E F G H
+              row, col
+        A8 -> (0,   0), A7 -> (1, 0),... A1 -> (7, 0)
+        B8 -> (0, 1),...etc.
+        """
+        pass
+
+    def get_from(self):
+        """Returns from coordinates"""
+        pass
+
+    def get_to(self):
+        """Returns to coordinates"""
+        pass
