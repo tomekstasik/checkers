7fd1469a13e41f2345ffbb1f4627d09de1bc4fa0
Tomek Stasik <tomekstasik01@gmail.com>
2020-07-26 11:40:57 +0200

    Fixing env creation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='checkers',
+    url='https://github.com/tomekstasik/checkers',
+    version='1.0',
+    description='Checkers game',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
